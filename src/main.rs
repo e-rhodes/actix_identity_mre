f2b9eac65e8ab0a24d5cc5de82e944bd85cba070
erhodes <erik@space-nav.com>
2022-07-15 16:33:13 -0600

    complete version showing issue

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,7 @@ use actix_web::{HttpMessage, HttpRequest, HttpResponse, Responder, web};
 #[actix_web::get("/login/{user}")]
 async fn login(req: HttpRequest, user: web::Path<String>) -> impl Responder {
     let user = user.into_inner();
-    println!("Login {user}");
+    log::debug!("Login {user}");
     let body = format!("<h1>logged in {user}</h1>");
     Identity::login(&req.extensions(), user).unwrap();
     HttpResponse::Ok().body(body)
@@ -16,7 +16,7 @@ async fn login(req: HttpRequest, user: web::Path<String>) -> impl Responder {
 
 #[actix_web::get("/logout")]
 async fn logout(id: Identity) -> impl Responder {
-    println!("Logout");
+    log::debug!("Logout");
     let user = id.id().unwrap();
     id.logout();
     let body = format!("<h1>logged out {user}</h1>");
@@ -29,26 +29,33 @@ async fn index(id: Option<Identity>) -> impl Responder {
     } else {
         String::from("not logged in")
     };
-    println!("{}", msg);
+    log::debug!("{}", msg);
     let body = format!("<h1>{msg}</h1>");
     HttpResponse::Ok().body(body)
 }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    simple_logger::init_with_level(log::Level::Debug).unwrap();
     let key = Key::generate();
-
     let mut server = HttpServer::new(move || {
         App::new()
-            .service(index)
-            .service(login)
-            .service(logout)
-            // .wrap(IdentityMiddleware::default())
+            .wrap(IdentityMiddleware::builder()
+                .visit_deadline(Some(std::time::Duration::from_secs(2 * 86400)))
+                .build()
+            )
             .wrap(SessionMiddleware::new(
                 CookieSessionStore::default(),
                 key.clone(),
             ))
-            .wrap(IdentityMiddleware::default())
+            // .wrap(IdentityMiddleware::builder()
+            //     .visit_deadline(Some(std::time::Duration::from_secs(2 * 86400)))
+            //     .build()
+            // ) // works, but complains on every request
+            .wrap(actix_web::middleware::Logger::default())
+            .service(index)
+            .service(login)
+            .service(logout)
     });
     let mut listenfd = listenfd::ListenFd::from_env();
     server = match listenfd.take_tcp_listener(0)? {
